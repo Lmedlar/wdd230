1835255447e2bb1463d5f03f86b41c8f3a64fee6
Lmedlar <med20017@byui.edu>
2021-09-18 10:23:09 -0700

    editing!!

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let now = new Date();
 let year = now.getFullYear();
-document.getElementById("currentyear").innerHTML = year;
+document.getElementById("currentyear").innerHTML = `${year} | Lanea Charlene Medlar | California | `;
 
 let mod = document.lastModified;
 document.getElementById("modDate").innerHTML = `Last Modified: ${mod}`;
