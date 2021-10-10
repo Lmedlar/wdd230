205b3dc6695c92b1bdcf6ae365d3a283dfe081d8
Lmedlar <med20017@byui.edu>
2021-10-10 16:04:10 -0700

    bookofmormon chapters

---
+++ js/bom.js
@@ -0,0 +1,24 @@
+const inp = document.querySelector ('input');
+const button = document.querySelector ('button');
+const list = document.querySelector ('ul');
+
+button.addEventListener ('click', function() {
+   let input = inp.value;
+   inp.value = '';
+
+    const item = document.createElement('li');
+    const chapter = document.createElement('span');
+    const press = document.createElement('button');
+
+   item.appendChild(chapter);
+   chapter.textContent = input;
+   item.appendChild(press);
+   press.textContent = 'Delete';
+   list.appendChild(item);
+
+   press.onclick = function(e) {
+       list.removeChild(item);
+   }
+
+   input.focus();
+} )
