c6e7c447d17b1a98dc5d5423ec727772dd7d2807
Lmedlar <med20017@byui.edu>
2021-10-14 12:32:50 -0700

    banner and 5day

--- lesson5/js/preston.js
+++ lesson5/js/preston.js
@@ -4,3 +4,12 @@ document.getElementById("date").innerHTML = date.toDateString();
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+let banner = document.getElementById("banner");
+let day = date.getDay();
+
+if (day == 5) {
+    banner.style.display = 'block';
+} else {
+    banner.style.display = 'none';
+}
