11eeaaba067816333e2745127f4e26b1461b041a
Lmedlar <med20017@byui.edu>
2021-10-27 18:24:58 -0700

    edts

--- lesson7/js/gallery.js
+++ lesson7/js/gallery.js
@@ -1,31 +1,31 @@
-let imgload = document.querySelectorAll("img[data-src]");
+let imagesToLoad = document.querySelectorAll("img[data-src]");
 
-let imgOptions = {
+const imgOptions = {
     threshold: 0,
     rootbargin: '0px 0px 50px 0px'
-};
+}
 
-const loadImg = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src-'));
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
         image.removeAttribute('data-src');
     };
 };
 
 if('IntersectionObserver' in window) {
-    const obs = new IntersectionObserver((items, obs) => {
+    const observer = new IntersectionObserver((items, observer) => {
         items.forEach((item) => {
             if(item.isIntersecting) {
-                loadImg(item.target);
-                obs.unobserve(item.target);
+                loadImages(item.target);
+                observer.unobserve(item.target);
             }
         }, imgOptions);
     });
-    imgload.forEach((img) => {
-        obs.observe(img);
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
     });
-}else {
-    imgload.forEach((img) => {
-        loadImg(img);
+    } else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
     });
 }
