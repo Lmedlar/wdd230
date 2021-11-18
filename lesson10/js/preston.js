a016a89985b393f80d4bb98b259a39cdf14766dc
Lmedlar <med20017@byui.edu>
2021-11-18 10:52:44 -0800

    weather updates

--- lesson10/js/preston.js
+++ lesson10/js/preston.js
@@ -13,3 +13,26 @@ if (day == 5) {
 } else {
     banner.style.display = 'none';
 }
+
+const apiURL="http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=8ad65ed96128107717960c21be193523"
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        document.getElementById('current').textContent=`${jsObject.main.temp} °F`;
+
+        document.getElementById('temp').textContent=`${jsObject.main.temp_max} °F`;
+
+        document.getElementById('humid').textContent=`${jsObject.main.humidity}%`;
+
+        document.getElementById('speed').textContent=`${jsObject.wind.speed}mph`;
+    });
+
+const forcastURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=8ad65ed96128107717960c21be193523"
+
+fetch(forcastURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+    });
--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,15 +1,12 @@
-let speed = parseFloat(document.getElementById("speed").innerHTML);
-let temp = parseFloat(document.getElementById("temp").innerHTML);
-let result;
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        let speed = JSON.parse(jsObject.wind.speed);
+        let temp = JSON.parse(jsObject.main.temp_max);
+        let result;
 
-if(speed == 3 && temp == 50) {
-    let speed2 = Math.pow(speed, 0.16);
-    result = 35.74 + (0.6215 * temp) - (35.75 * speed2) + (0.4275 * temp * speed2);
-    document.getElementById("chill").innerHTML = result.toFixed(2);
-} else {
-    result = "N/A"
-    document.getElementById("chill").innerHTML = result
-}
+        let speed2 = Math.pow(speed, 0.16);
+        result = 35.74 + (0.6215 * temp) - (35.75 * speed2) + (0.4275 * temp * speed2);
+        document.getElementById("chill").innerHTML = result.toFixed(2);
 
-
-
+    });
